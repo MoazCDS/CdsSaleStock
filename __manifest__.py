6b2d368fb936b2656be5692e93a5ea1120fcd3b5
Moaz Elbahr <moazelbahr@gmail.com>
2025-08-14 13:28:32 +0300

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CDS Sale Stock",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "CDS Solutions SRL",
+    'website': "www.cdsegypt.com",
+    'contributors': [
+        'Ramadan Khalil <rkhalil1990@gmail.com>',
+    ],
+    'version': '0.1',
+    'depends': ["base", "sale", "sale_stock", "stock", "account", "web"],
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    'license': 'OPL-1',
+    "pre_init_hook": None,
+    "post_init_hook": None,
+}
